301dcc8a2a75704189cfaefb8b694ad368c5e413
clinton <ameh.clinton@gmail.com>
2020-05-16 16:56:03 +0100

    commit

--- src/components/post/Posts.js
+++ src/components/post/Posts.js
@@ -1,25 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PostItem from "./PostItem";
 import Spinner from "../Layout/Spinner";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-
+import Pagination from "../Pagination";
 import { Link } from "react-router-dom";
 
 import { getPosts } from "../../actions/postActions";
 
 const Posts = ({ posts, loading, getPosts }) => {
+  const [showPage, setShowPage] = useState(10);
   useEffect(() => {
-    getPosts();
+    getPosts(showPage);
+
     // eslint-disable-next-line
   }, []);
 
+  const ShowPage = (showPage) => {
+    getPosts(showPage);
+  };
   if (loading) {
     return <Spinner />;
   } else {
     return (
       <div className='grid-3'>
-        {" "}
         {posts.map((post) => {
           return (
             <div>
@@ -29,6 +33,8 @@ const Posts = ({ posts, loading, getPosts }) => {
             </div>
           );
         })}
+
+        <Pagination pages={showPage} ShowPage={ShowPage} />
       </div>
     );
   }
@@ -39,6 +45,7 @@ Posts.propTypes = {
   posts: PropTypes.array.isRequired,
   getPosts: PropTypes.func.isRequired,
 };
+
 const mapStateToProps = (state) => ({
   posts: state.post.posts,
   loading: state.post.loading,
